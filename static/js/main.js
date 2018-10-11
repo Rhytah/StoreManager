105c0a5b8c0297f7e65052b3181b852b2a436fd0
Rhytah <ritanamono@gmail.com>
2018-10-11 04:04:38 +0300

    [Delivers #161078537]add shopping cart with functionality to add products to cart

--- static/js/main.js
+++ static/js/main.js
@@ -15,7 +15,57 @@ function hide(id) {
     d.style.display='block';
   }
 
-  //signup message 
-function singupAlert(){
-    alert("Sign up successful");
-}
+
+//create array that will hold all ordered products
+    var shoppingCart = [];
+
+    //this function manipulates DOM and displays content of our shopping cart
+    function displayShoppingCart(){
+        var orderedProductsTblBody=document.getElementById("orderedProductsTblBody");
+        //ensure we delete all previously added rows from ordered products table
+        while(orderedProductsTblBody.rows.length>0) {
+            orderedProductsTblBody.deleteRow(0);
+        }
+
+        //variable to hold total price of shopping cart
+        var cart_total_price=0;
+        //iterate over array of objects
+        for(var product in shoppingCart){
+            //add new row      
+            var row=orderedProductsTblBody.insertRow();
+            //create three cells for product properties 
+            var cellName = row.insertCell(0);
+            var cellDescription = row.insertCell(1);
+            var cellPrice = row.insertCell(2);
+            cellPrice.align="right";
+            //fill cells with values from current product 
+            cellName.innerHTML = shoppingCart[product].Name;
+            cellDescription.innerHTML = shoppingCart[product].Description;
+            cellPrice.innerHTML = shoppingCart[product].Price;
+            cart_total_price+=shoppingCart[product].Price;
+        }
+        //total sum of price
+        document.getElementById("cart_total").innerHTML=cart_total_price;
+    }
+
+
+    function AddtoCart(name,description,price){
+
+       var singleProduct = {};
+    
+       singleProduct.Name=name;
+       singleProduct.Description=description;
+       singleProduct.Price=price;
+       //Add newly created product to  cart 
+       shoppingCart.push(singleProduct);
+       //call display function to show on screen
+       displayShoppingCart();
+
+    }  
+    //display ordered items
+    function salerep(){
+        shoppingCart.tostring();
+        document.getElementById("sales").innerHTML=shoppingCart;
+
+    }
+
