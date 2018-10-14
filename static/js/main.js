0a3883b01c35261eb5b054cf1833eb86a25e155c
Rhytah <ritanamono@gmail.com>
2018-10-14 16:38:07 +0300

    [Delivers #161079330] add tab content with capability to add new attendant to existing attendants

--- static/js/main.js
+++ static/js/main.js
@@ -100,7 +100,37 @@ function opentab(evt, tabName ){
 }
 
 
+// add new attendant
+function addRow() {
+    var table = document.getElementById("storeattendants");
+    var row = table.insertRow(8);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    cell1.innerHTML = "New id";
+    cell2.innerHTML = "Store attendant name ";
+    cell3.innerHTML = "email";
+    cell4.innerHTML = "contact";
+    cell5.innerHTML = "Active";
+}
 
+//create sale record
+function createRecord() {
+    var table = document.getElementById("srecords");
+    var row = table.insertRow(6);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    
+    cell1.innerHTML = "New id";
+    cell2.innerHTML = "Store attendant name ";
+    cell3.innerHTML = "email";
+    cell4.innerHTML = "contact";
+    
+}
 
     
 //     var table=document.getElementById("srecords");
