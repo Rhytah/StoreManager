fed2561b02437b488abc5ce792530c8416d9cd25
Rhytah <ritanamono@gmail.com>
2018-10-15 09:15:54 +0300

    [Finishes #161079579] add input fields for new product

--- static/js/main.js
+++ static/js/main.js
@@ -131,6 +131,25 @@ function createRecord() {
     cell4.innerHTML = "contact";
     
 }
+// add new attendant
+function addProduct() {
+    var table = document.getElementById("products");
+    var row = table.insertRow(2);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    cell1.innerHTML = "New Product code";
+    cell2.innerHTML = "New Product ";
+    cell3.innerHTML = "New Product details";
+    cell4.innerHTML = "New Unit price";
+    cell5.innerHTML = "New Current-stock";
+    cell6.innerHTML = "New initial-stock";
+    cell7.innerHTML = "Minimum"
+}
 
     
 //     var table=document.getElementById("srecords");
